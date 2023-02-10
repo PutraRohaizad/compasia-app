30e7bf721f42a3e49cf5f9e063543884a9885786
Putra Rohaizad <putra@dwebly.com>
2023-02-10 15:05:24 +0800

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="m-5">
+    <input type="file">
+    <button class="btn btn-success">upload</button>
+  </div>
+  <br>
+  <hr>
+  <br>
+   <div class="m-5">
+     <h5>Product Master List</h5>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Product ID</th>
+                <th>Type</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Capacity</th>
+                <th>Quantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>100</td>
+                <td>100</td>
+                <td>100</td>
+                <td>100</td>
+                <td>100</td>
+                <td>100</td>
+                <td>100</td>
+            </tr>
+        </tbody>
+    </table>
+
+   </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import DataTable from 'datatables.net-vue3';
+
+createApp(App).mount('#app')
